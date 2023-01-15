723c0f23019a9494ff5fb6daf045976b885c2d00
hgkim87 <hgkim8704@gmail.com>
2023-01-15 23:09:10 +0900

    [FEAT] Race 상속 받는 CarRace생성

---
+++ src/main/java/service/CarRace.java
@@ -0,0 +1,30 @@
+package service;
+
+import service.domain.RaceCondition;
+import view.MonitorView;
+import view.View;
+
+public class CarRace implements Race {
+
+    private RaceCondition raceCondition;
+
+    public CarRace(RaceCondition raceCondition) {
+        this.raceCondition = raceCondition;
+    }
+
+    @Override
+    public void race() {
+        raceCondition.getCarList().forEach(car -> {
+            if (raceCondition.isAdvanceCondition()) car.move();
+        });
+    }
+
+    @Override
+    public void monitor() {
+        raceCondition.getCarList().forEach(car -> {
+            View view = new MonitorView();
+            String result = view.render(car);
+            System.out.println(result);
+        });
+    }
+}
