91e01c5cdb8130c8e599be0350f3014db2654b74
hgkim87 <hgkim8704@gmail.com>
2023-01-15 23:38:14 +0900

    [TEST] 각 테스트 코드 추가

---
+++ src/test/java/service/domain/CarTest.java
@@ -0,0 +1,22 @@
+package service.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @DisplayName("자동차 이동 시, 포지션 변경을 확인한다")
+    @Test
+    void carMoveTest() {
+        Car car = new Car("", 0);
+        car.move();
+        assertEquals(1, car.getPoisition());
+
+        car.move();
+        assertEquals(2, car.getPoisition());
+
+    }
+
+}
