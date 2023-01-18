1fdfc2fd85266fea06fc9a7626fd5db378c290fd
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-01-18 16:37:55 +0900

    [REFACTOR] 결과 뷰 분리 및, 화면 출력 개발

--- src/main/java/view/CarRaceResultView.java
+++ src/main/java/view/CarRaceResultView.java
@@ -1,36 +1,40 @@
 package view;
 
 
-import domain.vo.Car;
-
 import java.util.stream.Stream;
+
+import domain.vo.Car;
 
 public class CarRaceResultView implements View<String> {
 
-    private Stream<Car> cars;
+	private Stream<Car> cars;
+	private int tryNo;
 
-    public static CarRaceResultView from(Stream<Car> cars) {
-        return new CarRaceResultView(cars);
-    }
+	public static CarRaceResultView of(Stream<Car> cars, int tryNo) {
+		return new CarRaceResultView(cars, tryNo);
+	}
 
-    private CarRaceResultView(Stream<Car> cars) {
-        this.cars = cars;
-    }
+	private CarRaceResultView(Stream<Car> cars, int tryNo) {
+		this.cars = cars;
+		this.tryNo = tryNo;
+	}
 
-    @Override
-    public String render() {
-        StringBuilder view = new StringBuilder();
-        cars.forEach(car -> {
-            view.append(convertPositionToView(car.getPoisition())).append(System.lineSeparator());
-        });
-        return view.toString();
-    }
+	@Override
+	public String render() {
+		StringBuilder view = new StringBuilder();
+		view.append("======== ").append(tryNo).append("회차").append(" ========").append(System.lineSeparator());
+		cars.forEach(car -> {
+			view.append(convertPositionToView(car.getPoisition())).append(System.lineSeparator());
+		});
+		System.out.println(view);
+		return view.toString();
+	}
 
-    private String convertPositionToView(int position) {
-        StringBuilder positionView = new StringBuilder();
-        for (int n = 0; n < position; n++) {
-            positionView.append("-");
-        }
-        return positionView.toString();
-    }
+	private String convertPositionToView(int position) {
+		StringBuilder positionView = new StringBuilder();
+		for (int n = 0; n < position; n++) {
+			positionView.append("-");
+		}
+		return positionView.toString();
+	}
 }
