709c8f59dc6c6385ca7b9fdac215abbf83238ec4
hgkim87 <hgkim8704@gmail.com>
2023-01-18 23:53:17 +0900

    [FIX] List<Car> 딥카피 메서드 추가

--- src/main/java/domain/CarRace.java
+++ src/main/java/domain/CarRace.java
@@ -5,52 +5,61 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import domain.vo.Car;
 
 public class CarRace implements Race<Stream<Car>> {
 
-	private List<Car> cars = new ArrayList<>();
-	private final CarRaceDifficulty carRaceDifficulty;
-	private Map<Integer, Stream<Car>> carRaceStatus = new HashMap<>();
+    private List<Car> cars = new ArrayList<>();
+    private final CarRaceDifficulty carRaceDifficulty;
+    private Map<Integer, Stream<Car>> carRaceStatus = new HashMap<>();
 
-	public static CarRace readyForRace(int carCount, CarRaceDifficulty carRaceDifficulty) {
-		List<Car> newCars = createNewRacingCar(carCount);
-		return new CarRace(newCars, carRaceDifficulty);
-	}
+    public static CarRace readyForRace(int carCount, CarRaceDifficulty carRaceDifficulty) {
+        List<Car> newCars = createNewRacingCar(carCount);
+        return new CarRace(newCars, carRaceDifficulty);
+    }
 
-	private CarRace(List<Car> cars, CarRaceDifficulty carRaceDifficulty) {
-		this.cars = cars;
-		this.carRaceDifficulty = carRaceDifficulty;
-	}
+    private CarRace(List<Car> cars, CarRaceDifficulty carRaceDifficulty) {
+        this.cars = cars;
+        this.carRaceDifficulty = carRaceDifficulty;
+    }
 
-	private static List<Car> createNewRacingCar(int carCount) {
-		List<Car> newCars = new ArrayList<>();
-		for (int cnt = 0; cnt < carCount; cnt++) {
-			newCars.add(new Car("", 0));
-		}
-		return newCars;
-	}
+    private static List<Car> createNewRacingCar(int carCount) {
+        List<Car> newCars = new ArrayList<>();
+        for (int cnt = 0; cnt < carCount; cnt++) {
+            newCars.add(Car.of("", 0));
+        }
+        return newCars;
+    }
 
-	@Override
-	public void race(int tryNo) {
-		cars.forEach(car -> {
-			move(car);
-		});
+    @Override
+    public void race(int tryNo) {
+        cars.forEach(car -> {
+            move(car);
+        });
 
-		carRaceStatus.put(tryNo, cars.stream());
-	}
+        carRaceStatus.put(tryNo, copyOf(cars).stream());
+    }
 
-	private void move(Car car) {
-		Random random = new Random();
-		if (carRaceDifficulty.isMoveOk(random.nextInt(10))) {
-			car.move();
-		}
-	}
+    private void move(Car car) {
+        Random random = new Random();
+        if (carRaceDifficulty.isMoveOk(random.nextInt(10))) {
+            car.move();
+        }
+    }
 
-	@Override
-	public Stream<Car> getRaceStatus(int tryNo) {
-		return this.carRaceStatus.get(tryNo);
-	}
+    private List<Car> copyOf(List<Car> cars) {
+        List<Car> newCars = new ArrayList<>();
+        for(Car car : cars) {
+            newCars.add(car.copy());
+        }
+        return newCars;
+    }
+
+    @Override
+    public Stream<Car> getRaceStatus(int tryNo) {
+        return this.carRaceStatus.get(tryNo);
+    }
 }
