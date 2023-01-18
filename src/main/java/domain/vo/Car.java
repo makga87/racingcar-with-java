59a2c6f361411bf02fcadf5fd2bc07ba74a4964a
hgkim87 <hgkim8704@gmail.com>
2023-01-18 23:52:11 +0900

    [FIX] 상태값이 상속되는 문제로, Car객체 카피하여 새로운 객체로 생성하는 로직 개발 [REFACTOR] Car 객체, 정적팩토리메서드로 변경

--- src/main/java/domain/vo/Car.java
+++ src/main/java/domain/vo/Car.java
@@ -5,7 +5,11 @@ public class Car {
     private String carName;
     private int poisition;
 
-    public Car(String carName, int poisition) {
+    public static Car of(String carName, int poisition) {
+        return new Car(carName, poisition);
+    }
+
+    private Car(String carName, int poisition) {
         this.carName = carName;
         this.poisition = poisition;
     }
@@ -16,5 +20,14 @@ public class Car {
 
     public int getPoisition() {
         return this.poisition;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Car copy() {
+        return new Car(this.carName, this.poisition);
     }
 }
