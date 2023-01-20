8865c45424af6fb61c72cad4a4bf48f5ef947320
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-01-20 16:53:53 +0900

    [REFACTOR] 자동차 객체가 외부조건이 아닌 자신이 주체적으로 이동해야한다 하여 해당 방식으로 수정

--- src/main/java/domain/vo/Car.java
+++ src/main/java/domain/vo/Car.java
@@ -1,28 +1,36 @@
 package domain.vo;
 
+import java.util.Random;
+
+import domain.CarRaceDifficulty;
+
 public class Car {
 
-    private String carName;
-    private int poisition;
+	private String carName;
+	private int poisition;
 
-    public static Car of(String carName, int poisition) {
-        return new Car(carName, poisition);
-    }
+	public static Car of(String carName, int poisition) {
+		return new Car(carName, poisition);
+	}
 
-    private Car(String carName, int poisition) {
-        this.carName = carName;
-        this.poisition = poisition;
-    }
+	private Car(String carName, int poisition) {
+		this.carName = carName;
+		this.poisition = poisition;
+	}
 
-    public void move() {
-        this.poisition++;
-    }
+	public void move(CarRaceDifficulty carRaceDifficulty) {
+		Random random = new Random();
+		move(carRaceDifficulty, random.nextInt(carRaceDifficulty.getRandomRange()));
+	}
 
-    public int getPoisition() {
-        return this.poisition;
-    }
+	public void move(CarRaceDifficulty carRaceDifficulty, int randomValue) {
+		int moveCondition = carRaceDifficulty.getMoveCondition();
+		if (moveCondition <= randomValue) {
+			this.poisition++;
+		}
+	}
 
-    public Car copy() {
-        return new Car(this.carName, this.poisition);
-    }
+	public int getPoisition() {
+		return this.poisition;
+	}
 }
