94f539e452fca257b6d2654eaa7ec3f13c70e9f4
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-01-30 20:47:10 +0900

    [ADD] CarTest추가

--- src/test/java/domain/vo/CarTest.java
+++ src/test/java/domain/vo/CarTest.java
@@ -2,23 +2,32 @@ package domain.vo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import domain.CarRaceDifficulty;
 
 class CarTest {
 
+	@RepeatedTest(10)
 	@DisplayName("자동차 이동 시, 포지션 변경을 확인한다")
-	@Test
 	void carMoveTest() {
+
+		int expected = 0;
+
 		Car car = Car.of("", 0);
-		car.move(CarRaceDifficulty.NORMAL);
-		assertEquals(1, car.getPoisition());
+		CarRaceDifficulty carDifficulty = CarRaceDifficulty.NORMAL;
+		Random random = new Random();
 
-		car.move(CarRaceDifficulty.NORMAL);
-		assertEquals(2, car.getPoisition());
+		int randomValue = random.nextInt(carDifficulty.getRandomRange());
+		car.move(carDifficulty, randomValue);
 
+		if (carDifficulty.getMoveCondition() <= randomValue) {
+			expected++;
+		}
+
+		assertEquals(expected, car.getPoisition());
 	}
-
 }
