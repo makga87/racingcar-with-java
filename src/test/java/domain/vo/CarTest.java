1ed4097d65ec6e7f26f99b9698f02b37531d5cc9
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-01-30 20:52:56 +0900

    [ADD] Car.move 예외 로직 테스트 추가

--- src/test/java/domain/vo/CarTest.java
+++ src/test/java/domain/vo/CarTest.java
@@ -1,15 +1,33 @@
 package domain.vo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Random;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import domain.CarRaceDifficulty;
 
 class CarTest {
+
+	@DisplayName("자동차 이동에 대한 랜덤 값이 0보다 작은 경우 예외를 발생시킨다")
+	@CsvSource({"0", "-1"})
+	@ParameterizedTest
+	void carMoveErrorTest(int randomValue) {
+
+		Car car = Car.of("", 0);
+		CarRaceDifficulty carDifficulty = CarRaceDifficulty.NORMAL;
+
+		String message = assertThrows(IllegalArgumentException.class, ()->{
+			car.move(carDifficulty, randomValue);
+		}).getMessage();
+
+		assertEquals("Bad random value input", message);
+	}
 
 	@RepeatedTest(10)
 	@DisplayName("자동차 이동 시, 포지션 변경을 확인한다")
