98359530aff89b54780359e9785af24c4d698896
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-02-01 14:18:02 +0900

    [FIX] https://github.com/Effective-Tilinkle/racingcar-with-java/pull/4#discussion_r1092102587 컬렉션 조작 메서드 제거

--- src/main/java/domain/vo/Cars.java
+++ src/main/java/domain/vo/Cars.java
@@ -1,42 +1,29 @@
 package domain.vo;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+
+import domain.CarRaceDifficulty;
 
 public class Cars {
 
 	private List<Car> cars;
 
-	public static Cars from(List<Car> cars) {
-		return new Cars(cars);
-	}
-
-	public static Cars createInstance() {
-		return new Cars();
-	}
-
-	private Cars() {
-		this.cars = new ArrayList<>();
-	}
-
 	private Cars(List<Car> cars) {
 		this.cars = cars;
 	}
 
-	public Car getCar(int index) {
-		return this.cars.get(index);
+	public static Cars from(List<Car> cars) {
+		return new Cars(cars);
 	}
 
 	public Stream<Car> stream() {
 		return cars.stream();
 	}
 
-	public void addCar(Car car) {
-		this.cars.add(car);
-	}
-
-	public void removeCar(int index) {
-		this.cars.remove(index);
+	public void move(CarRaceDifficulty carRaceDifficulty) {
+		for (Car car : cars) {
+			car.move(carRaceDifficulty);
+		}
 	}
 }
