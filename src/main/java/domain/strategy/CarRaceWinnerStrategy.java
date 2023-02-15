d663a385ec821282b025e66f5d87c2815ece84be
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-02-15 14:20:35 +0900

    [FEAT] 우승자 결정 전략 인터페이스 구현체 추가 및 선두 객체 추가

---
+++ src/main/java/domain/strategy/CarRaceWinnerStrategy.java
@@ -0,0 +1,69 @@
+package domain.strategy;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import domain.vo.Car;
+import domain.vo.Cars;
+import domain.vo.FirstPlaceCar;
+
+public class CarRaceWinnerStrategy implements WinnerStrategy {
+
+	private Cars cars;
+	private List<Car> winners = new ArrayList<>();
+
+	private CarRaceWinnerStrategy() {
+	}
+
+	private CarRaceWinnerStrategy(Cars cars) {
+		this.cars = cars;
+	}
+
+	public static CarRaceWinnerStrategy from(Cars cars) {
+		return new CarRaceWinnerStrategy(cars);
+	}
+
+	@Override
+	public String winner() {
+		Stream<Car> sortedCars = sort(cars);
+		decideWinner(sortedCars);
+		return winnerToString();
+	}
+
+
+	private String winnerToString() {
+		StringBuilder sb = new StringBuilder();
+		for (Car winner : winners) {
+			sb.append(winner.getCarName()).append(",");
+		}
+
+		return sb.deleteCharAt(sb.length() - 1).toString();
+	}
+
+	private Stream<Car> sort(Cars cars) {
+		return cars.stream()
+				   .sorted(Comparator.comparing(Car::getPosition));
+	}
+
+	private void decideWinner(Stream<Car> sortedCars) {
+
+		FirstPlaceCar firstPlaceCar = FirstPlaceCar.init();
+
+		sortedCars
+				.takeWhile(car -> car.getPosition() >= firstPlaceCar.getPosition())
+				.forEach(car -> {
+					if (firstPlaceCar.isOvertake(car.getPosition())) {
+						winners.clear();
+						winners.add(car);
+						firstPlaceCar.changeFirstPlace(car);
+					}
+
+					if (firstPlaceCar.isCatchUpWith(car.getPosition()) && firstPlaceCar.isDifferentCar(car.getCarName())) {
+						winners.add(car);
+					}
+				});
+	}
+
+}
