0cdcd543089fd9d4f2200dbed6e81e7d6ea22485
Kim HyungGeun <hyunggeun.kim@nhnsoft.com>
2023-02-21 11:52:50 +0900

    [FIX] https://github.com/Effective-Tilinkle/racingcar-with-java/pull/9#discussion_r1112024242

--- src/test/java/domain/vo/CarTest.java
+++ src/test/java/domain/vo/CarTest.java
@@ -1,9 +1,14 @@
 package domain.vo;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import domain.CarRaceDifficulty;
+import domain.strategy.CarMoveStrategy;
+import domain.strategy.MoveStrategy;
 
 class CarTest {
 
@@ -21,5 +26,20 @@ class CarTest {
 		assertThrows(IllegalArgumentException.class, () -> {
 			Car car = Car.from("abcdef");
 		});
+	}
+
+	@DisplayName("자동차가 이동 가능한상태인 경우에만 이동한다")
+	@RepeatedTest(5)
+	@Test
+	void movableTest() {
+
+		MoveStrategy moveStrategy = CarMoveStrategy.from(CarRaceDifficulty.NORMAL);
+		boolean movable = moveStrategy.movable();
+
+		Car car = Car.from("abc");
+		car.move(movable);
+
+		if(movable) assertEquals(1, car.getPosition());
+		if(!movable) assertEquals(0, car.getPosition());
 	}
 }
