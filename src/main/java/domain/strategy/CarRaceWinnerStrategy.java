49e90c0b5c5993bd786dad2cd246b54b058e56bd
hgkim87 <hgkim8704@gmail.com>
2023-03-04 17:54:20 +0900

    [FIX] https://github.com/Effective-Tilinkle/racingcar-with-java/pull/9#discussion_r1112038889

--- src/main/java/domain/strategy/CarRaceWinnerStrategy.java
+++ src/main/java/domain/strategy/CarRaceWinnerStrategy.java
@@ -1,68 +1,56 @@
 package domain.strategy;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Stream;
-
 import domain.vo.Car;
 import domain.vo.Cars;
-import domain.vo.FirstPlaceCar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarRaceWinnerStrategy implements WinnerStrategy {
 
-	private Cars cars;
-	private List<Car> winners = new ArrayList<>();
+    private Cars cars;
+    private List<Car> winners = new ArrayList<>();
 
-	private CarRaceWinnerStrategy(Cars cars) {
-		this.cars = cars;
-	}
+    private CarRaceWinnerStrategy(Cars cars) {
+        this.cars = cars;
+    }
 
-	public static CarRaceWinnerStrategy from(Cars cars) {
-		return new CarRaceWinnerStrategy(cars);
-	}
+    public static CarRaceWinnerStrategy from(Cars cars) {
+        return new CarRaceWinnerStrategy(cars);
+    }
 
-	@Override
-	public String winner() {
-		Stream<Car> sortedCars = sort(cars);
-		decideWinner(sortedCars);
-		return winnerToString();
-	}
+    @Override
+    public String winner() {
+        decideWinner();
+        return winnerToString();
+    }
 
 
-	private String winnerToString() {
-		StringBuilder sb = new StringBuilder();
-		for (Car winner : winners) {
-			sb.append(winner.getCarName()).append(",");
-		}
+    private String winnerToString() {
+        StringBuilder sb = new StringBuilder();
+        for (Car winner : winners) {
+            sb.append(winner.getCarName()).append(",");
+        }
 
-		return sb.deleteCharAt(sb.length() - 1).toString();
-	}
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
 
-	private Stream<Car> sort(Cars cars) {
-		return cars.stream()
-				   .sorted(Comparator.comparing(Car::getPosition));
-	}
 
-	private void decideWinner(Stream<Car> sortedCars) {
+    private void decideWinner() {
 
-		FirstPlaceCar firstPlaceCar = FirstPlaceCar.init();
+        Car firstPlaceCar = cars.sortByPosition().findFirst().get();
+        winners.add(firstPlaceCar);
 
-		sortedCars.takeWhile(car -> car.getPosition() >= firstPlaceCar.getPosition())
-				  .forEach(car -> {
-					  changeFirstPlace(firstPlaceCar, car);
-				  });
-	}
+        cars.sortByPosition()
+                .skip(1)
+                .forEach(car -> {
+                    addWinner(firstPlaceCar, car);
+                });
 
-	private void changeFirstPlace(FirstPlaceCar firstPlaceCar, Car car) {
-		if (firstPlaceCar.isPass(car.getPosition())) {
-			winners.clear();
-			winners.add(car);
-			firstPlaceCar.changeFirstPlace(car);
-		}
+    }
 
-		if (firstPlaceCar.isOvertake(car.getPosition()) && firstPlaceCar.isDifferentCar(car.getCarName())) {
-			winners.add(car);
-		}
-	}
+    private void addWinner(Car firstPlaceCar, Car car) {
+        if (car.getPosition() == firstPlaceCar.getPosition())
+            winners.add(car);
+    }
 }
