79145e26074c0a93e9073c7baa4e24fe1fd90b4a
hgkim87 <hgkim8704@gmail.com>
2023-03-04 17:54:09 +0900

    [FIX] https://github.com/Effective-Tilinkle/racingcar-with-java/pull/9#discussion_r1112038889

--- src/main/java/domain/vo/Cars.java
+++ src/main/java/domain/vo/Cars.java
@@ -1,50 +1,57 @@
 package domain.vo;
 
+import domain.strategy.MoveStrategy;
+import utils.StringUtils;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import domain.strategy.MoveStrategy;
-import utils.StringUtils;
-
 public class Cars {
 
-	private List<Car> cars;
+    private List<Car> cars;
 
-	private Cars(List<Car> cars) {
-		this.cars = cars;
-	}
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
 
-	public static Cars from(List<Car> cars) {
-		return new Cars(cars);
-	}
+    public static Cars from(List<Car> cars) {
+        return new Cars(cars);
+    }
 
-	public static Cars from(String carNames) {
+    public static Cars from(String carNames) {
 
-		validateCarNames(carNames);
+        validateCarNames(carNames);
 
-		String[] carNameArray = carNames.split(",");
+        String[] carNameArray = carNames.split(",");
 
-		return new Cars(Stream.of(carNameArray)
-							  .map(String::trim)
-							  .map(Car::from)
-							  .collect(Collectors.toList()));
-	}
+        return new Cars(Stream.of(carNameArray)
+                .map(String::trim)
+                .map(Car::from)
+                .collect(Collectors.toList()));
+    }
 
-	public Stream<Car> stream() {
-		return cars.stream();
-	}
+    private static void validateCarNames(String carNames) {
 
-	public void move(MoveStrategy moveStrategy) {
-		for (Car car : cars) {
-			car.move(moveStrategy.movable());
-		}
-	}
+        if (StringUtils.isEmpty(carNames)) {
+            throw new IllegalArgumentException("자동차 명이 입력되지 않았습니다");
+        }
+    }
 
-	private static void validateCarNames(String carNames) {
+    public Stream<Car> stream() {
+        return cars.stream();
+    }
 
-		if (StringUtils.isEmpty(carNames)) {
-			throw new IllegalArgumentException("자동차 명이 입력되지 않았습니다");
-		}
-	}
+    public void move(MoveStrategy moveStrategy) {
+        for (Car car : cars) {
+            car.move(moveStrategy.movable());
+        }
+    }
+
+    public Stream<Car> sortByPosition() {
+        return cars.stream()
+                .sorted(Comparator.comparing(Car::getPosition).reversed());
+    }
+
 }
